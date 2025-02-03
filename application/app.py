c6576c5268e9b9fd41775b7f0e5f7c7cbfe6de98
starboundstitch <personal@stitchy.moe>
2025-02-03 19:10:16 +0000

    chore(app): format python

--- application/app.py
+++ application/app.py
@@ -2,18 +2,22 @@ import serial
 import sys
 import time
 
+
 def write_read(x):
-    arduino.write(bytes(x,  'utf-8'))
+
+    cmd = x + "\r"
+    print(str.encode(cmd))
+    arduino.write(bytes(cmd, "utf-8"))
     time.sleep(0.05)
     data = arduino.readline()
-    return  data
+    return data
+
 
 if __name__ == "__main__":
-  
-  arduino = serial.Serial(port=sys.argv[1],  baudrate=9600, timeout=.1)
 
-  while True:
-    num = input("Enter a number: ")
-    value  = write_read(num)
-    print(value)
+    arduino = serial.Serial(port=sys.argv[1], baudrate=9600, timeout=0.1)
 
+    while True:
+        num = input("Enter a number: ")
+        value = write_read(num)
+        print(value)
